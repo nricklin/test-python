068d0868e28831c7c436fe6163453878e7f8e9dc
Jeff Naus <jeffnaus@gmail.com>
2016-01-25 10:01:39 -0700

    bumping version

--- test-package/setup.py
+++ test-package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='test-package',
-      version='0.1.1',
+      version='0.1.2',
       author='jnaus',
       author_email='jnaus@digitalglobe.com',
       license='MIT',

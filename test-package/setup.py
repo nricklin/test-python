5b50cf37650ca32e4ee757b6949256e54183b8d4
Nate Ricklin <nate@rickl.in>
2016-01-26 17:00:53 -0700

    Bump version: 0.2.3 → 1.0.0

--- test-package/setup.py
+++ test-package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='test-package',
-      version='0.2.3',
+      version='1.0.0',
       author='jnaus',
       author_email='jnaus@digitalglobe.com',
       license='MIT',
